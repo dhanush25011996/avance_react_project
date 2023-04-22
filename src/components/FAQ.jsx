defed3116cb7f160c07883f4d6f73b1d5b1bd962
Dhanush Kodi <dhanushvijayakumar@gmail.com>
2023-04-22 17:52:05 +0530

    Footer and Faq adjusted

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -2,28 +2,28 @@ import React from "react";
 
 const FAQ = () => {
   return (
-    <div className="text-center my-24">
-      <h2 className="text-xl font-semibold text-green-500 mb-4">FAQ</h2>
-      <h1 className="text-8xl font-bold mb-4">Still have questions?</h1>
-      <p className="mb-6 mt-16">
+    <div className="text-center my-8 sm:my-24">
+      <h2 className="text-lg sm:text-xl font-semibold text-green-500 mb-2 sm:mb-4">FAQ</h2>
+      <h1 className="text-3xl sm:text-8xl font-bold mb-2 sm:mb-4">Still have questions?</h1>
+      <p className="mb-4 sm:mb-6 mt-6 sm:mt-16">
         Cryptocurrency markets have seen an increase in volume in recent weeks,
         which is great opportunity for new traders
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-8 mb-6 mt-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 my-4 sm:my-8 mb-6 sm:mb-0 mt-6 sm:mt-16">
         <div>
-          <h4 className="text-xl font-bold mb-2">
+          <h4 className="text-lg sm:text-xl font-bold mb-2">
             What is blockchain technology?
           </h4>
-          <h4 className="text-xl font-bold mb-2 my-12">What is bitcoin?</h4>
-          <h4 className="text-xl font-bold mb-2 my-12">Who created bitcoin?</h4>
+          <h4 className="text-lg sm:text-xl font-bold mb-2 my-6 sm:my-12">What is bitcoin?</h4>
+          <h4 className="text-lg sm:text-xl font-bold mb-2 my-6 sm:my-12">Who created bitcoin?</h4>
         </div>
         <div>
-          <h4 className="text-xl font-bold mb-2">What is cryptocurrency?</h4>
-          <h4 className="text-xl font-bold mb-2 my-12">
-            How does Crptocurrency wallet works?
+          <h4 className="text-lg sm:text-xl font-bold mb-2">What is cryptocurrency?</h4>
+          <h4 className="text-lg sm:text-xl font-bold mb-2 my-6 sm:my-12">
+            How does cryptocurrency wallet work?
           </h4>
-          <h4 className="text-xl font-bold mb-2 my-12">
-            How to create cryptocurrency wallet?
+          <h4 className="text-lg sm:text-xl font-bold mb-2 my-6 sm:my-12">
+            How to create a cryptocurrency wallet?
           </h4>
         </div>
       </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,45 +2,44 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="text-center">
-      <div className="mx-auto flex flex-wrap md:flex-nowrap justify-between py-4 md:py-12">
-        <div className="md:w-2/5 sm:w-full mb-4 md:mb-0">
-          <h4 className="font-bold text-xl mb-2">Toka</h4>
-          <p className="font-medium text-base md:pr-24 pl-4">
+    <div className="text-center bg-blueGray-900 text-whitesmoke">
+      <div className="mx-auto flex flex-wrap justify-between py-6 md:py-10">
+        <div className="md:w-1/3 w-full mb-4 md:mb-0">
+          <h4 className="font-bold text-xl mb-4">Toka</h4>
+          <p className="font-medium text-base">
             Reinventing the way of creating websites, we aim to create the most master-peaced WordPress theme available on the market.
           </p>
         </div>
 
-        <div className="md:w-3/10 sm:w-1/2 mb-4 md:mb-0">
-          <h6 className="font-semibold mb-2 text-sm">CONTACT US</h6>
+        <div className="md:w-1/3 w-full mb-4 md:mb-0">
+          <h6 className="font-semibold mb-2 text-base">CONTACT US</h6>
           <p className="text-base mb-2">202 Helga Springs Rd, Crawford, TN 38554</p>
           <p className="font-semibold text-base mb-2 cursor-pointer hover:text-cyan-500 transition-colors">Call Us: 800.275.8777</p>
           <p className="text-base mb-2 cursor-pointer hover:text-cyan-500 transition-colors">alex@company.com</p>
         </div>
 
-        <div className="md:w-3/10 sm:w-1/2 mb-4 md:mb-0">
-          <h6 className="font-semibold mb-2 text-sm">SIGN UP FOR EMAIL UPDATES</h6>
-          <div className="flex items-center mb-2">
-            <input type="email" className="bg-blueGray-800 py-2 px-4 rounded-full w-full" />
-            <button className="bg-emerald-500 text-white py-2 px-4 rounded-full ml-2 hover:bg-emerald-600">Subscribe</button>
+        <div className="md:w-1/3 w-full mb-4 md:mb-0">
+          <h6 className="font-semibold mb-2 text-base">SIGN UP FOR EMAIL UPDATES</h6>
+          <div className="flex items-center">
+            <input type="email" className="bg-blueGray-800 py-2 px-4 rounded-l-full w-full" placeholder="Enter your email" />
+            <button className="bg-emerald-500 text-whitesmoke py-2 px-4 rounded-r-full hover:bg-emerald-600 transition-colors">Subscribe</button>
           </div>
         </div>
       </div>
 
-      <div className="container mx-auto flex flex-wrap md:flex-nowrap justify-between py-4">
-        <div className="md:w-1/3 sm:w-full md:text-left text-center mb-4 md:mb-0">
-          <p className="text-whitesmoke font-medium text-sm sm:text-base md:pl-20 pt-4">Copyright ©2022 Toka.
-            All rights reserved.</p>
+      <div className="mx-auto flex flex-wrap justify-between py-4 md:py-6">
+        <div className="md:w-1/3 w-full md:text-left text-center mb-4 md:mb-0">
+          <p className="text-sm md:text-base">&copy; 2023 Toka. All rights reserved.</p>
         </div>
 
-        <div className="md:w-1/3 sm:w-1/2 lg:w-1/4 text-center md:text-right">
-          <ul className="text-xs sm:text-sm flex flex-col md:flex-row justify-between md:mr-12">
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Our Team</li>
-            <li>Shop</li>
-            <li>Blog</li>
-            <li>Contact Us</li>
+        <div className="md:w-1/3 w-full text-center">
+          <ul className="text-xs md:text-sm flex flex-col md:flex-row justify-center md:justify-end">
+            <li className="mb-2 md:mb-0 md:mr-6"><a href="#">Home</a></li>
+            <li className="mb-2 md:mb-0 md:mr-6"><a href="#">About Us</a></li>
+            <li className="mb-2 md:mb-0 md:mr-6"><a href="#">Our Team</a></li>
+            <li className="mb-2 md:mb-0 md:mr-6"><a href="#">Shop</a></li>
+            <li className="mb-2 md:mb-0 md:mr-6"><a href="#">Blog</a></li>
+            <li><a href="#">Contact Us</a></li>
           </ul>
         </div>
       </div>
